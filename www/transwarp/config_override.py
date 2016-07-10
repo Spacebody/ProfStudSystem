aa92ffba43cd05f221b48c1506af65980daebe54
lightsing <light.tsing@gmail.com>
2016-07-10 15:25:53 +0800

    Add Web Framework and Configure File 	new file:   www/transwarp/config.py 	new file:   www/transwarp/config_default.py 	new file:   www/transwarp/config_override.py 	new file:   www/transwarp/web.py

---
+++ www/transwarp/config_override.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Override configurations.
+'''
+
+__author__ = 'Lightsing'
+
+configs = {
+    'db': {
+        'host': '10.20.2.113',
+        'port': 3306,
+        'user': 'develop_root',
+        'password': 'PythonDatabase',
+        'database': 'develop'
+    },
+    'session': {
+        'secret': 'y9aB5J"kU}O28u0'
+    }
+}
